633df8988b4355c61d0fb5c11d054d738400f423
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-04-23 08:50:32 +0300

    Scripting the contact to mailchimp

---
+++ scripts.js
@@ -0,0 +1,11 @@
+(function($) {window.fnames = new Array(); 
+    window.ftypes = new Array();
+    fnames[1]='FNAME';
+    ftypes[1]='text';
+    fnames[0]='EMAIL';
+    ftypes[0]='email';
+    fnames[2]='MMERGE2';
+    ftypes[2]='text';
+}
+(jQuery));
+var $mcj = jQuery.noConflict(true);
