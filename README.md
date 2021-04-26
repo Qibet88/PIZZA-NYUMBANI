36d425b20d8697fbfebd31864773c78e1ddd77aa
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-04-26 09:54:46 +0300

    Adding a readme file

---
+++ README.md
@@ -0,0 +1,17 @@
+# PIZZA NYUMBANI
+#### This is a platform for one to order pizza easily, {April 4, 2021}
+####  **Contribution by Moringa School**
+## Description
+This allows a customer to select their choice of pizza, crust and toppings and choose whether they need it delivered to their residence.
+## Setup/Installation Requirements
+* Liveserver is needed to be installed
+## Known Bugs
+No known bugs yet.  Incase of any bugs feel free to edit it through my github account through; https://github.com/Qibet88/PIZZA-NYUMBANI
+## Technologies Used
+Website coded using HTML,CSS,Javascript, Jquery
+## Support and contact details
+Incase of any bottlenecks or challenges, Kindly reach us on Email: bernardonline88@gmail.com
+Github access link: 
+### License
+This project is under the  [MIT](LICENSE) license
+Copyright (c) 2021                          Developer: Bernard Kibet
--- scripts.js
+++ scripts.js
@@ -9,3 +9,63 @@
 }
 (jQuery));
 var $mcj = jQuery.noConflict(true);
+function Order(size, crust, topping) {
+    this.size = size;
+    this.crust = crust;
+    this.topping = toppings;
+}
+Order.prototype.completeOrder = function () {
+    return this.size + " with the crust of " + this.crust + " and " + this.toppings + " as toppings.";
+};
+function Charge(price, quantity, delivery) {
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
+}
+
+Charge.prototype.finalCharge = function () {
+    return this.price * this.quantity + this.delivery;
+};
+
+
+var sizePrice = [750, 1000, 600]
+var deliverPrices = [0, 300];
+
+$(document).ready(function () {
+    $('.orderForm').submit(function (event) {
+        event.preventDefault();
+       
+
+        var pizzaSize = parseInt($('#size').val());
+
+        var pizzaCrust = $('#crust').val();
+
+        var pizzaTop = $('#toppings').val();
+
+        var pizzaQty = parseInt($('#number').val());
+
+        var pizzaPick = parseInt($('#pick').val());
+
+
+        var price = sizePrice[pizzaSize - 1];
+
+
+        var DeliveryCost = deliverPrices[pizzaPick - 1];
+
+
+
+        newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
+        newTotal = new Total(price, pizzaQty, DeliveryCost);
+        if (pizzaPick===1){
+        alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+        alert("your bill is: " + newTotal.finalTotal());
+        }else{
+            if(pizzaPick===2){
+                prompt("Enter where you want your pizza to be delivered");
+                alert("Your order has been received and it will be delivered. Continue to see your order details");
+                alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+                alert("your bill is: " + newTotal.finalTotal());
+            }
+        }
+
+    });
